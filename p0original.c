0f02bc5acf23f1406a761541834dcbbf7d35389e
Sindhu Priya Korrapati <94669334+SindhuPriyaKorrapatiMuni@users.noreply.github.com>
2021-11-29 07:02:09 +0000

    done

--- p0original.c
+++ p0original.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-  char a[11];
-  printf("enter your name\n");
+  char a[6];
+  printf("enter your name:\n");
   scanf("%s",a);
   printf("your name is:%s",a);
   return 0;
 }
-
